fdf595378fe38918350eced218bd831e20c7bd60
ale <ale@rsglab.com>
2018-04-02 10:27:44 -0400

    add: language dropdown

--- src/App.js
+++ src/App.js
@@ -6,36 +6,52 @@ import {Controlled as CodeMirror} from 'react-codemirror2'
 import 'codemirror/lib/codemirror.css';  
 import 'codemirror/theme/monokai.css';  
 import 'codemirror/mode/javascript/javascript.js';
+import 'codemirror/mode/python/python';
+import 'codemirror/mode/ruby/ruby';
+import 'codemirror/mode/clike/clike';
+import 'codemirror/mode/go/go';
 const db = fire.database()
 
 class App extends Component {
   constructor() {
     super()
-    this.state = {value: ''}
+    this.state = {
+      value: '',
+      language: '',
+    }
   }
+  
   handleChange = (newValue) => {
     this.setState({
       value: newValue
     })
-    db.ref(`/room`).set({
-      value: this.state.value
+    db.ref(`/room`).update({
+      value: this.state.value,
     })
   }
+  
   componentDidMount() {
     db.ref(`/room/value`).on('value', snapshot => {
       const value = snapshot.val()
       this.setState({value})
     })
+    db.ref(`/room/language`).on('value', snapshot => {
+      const language = snapshot.val()
+      console.log('language in App componentDidmount', language)
+      this.setState({language})
+    })
   }
   render() {
     const options = {
       lineNumbers: true,
-      mode: 'javascript',
+      mode: this.state.language,
       theme: 'monokai',
     }
     return (
       <div className="App">
-        <header><Menu /></header>
+        <header>
+          <Menu />
+        </header>
         <CodeMirror
           value={this.state.value} 
           options={options}
--- src/Menu.js
+++ src/Menu.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react'
-import { Menu, Button } from 'semantic-ui-react'
+import { Menu, Button, Dropdown } from 'semantic-ui-react'
+import fire from './firebase';
+const db = fire.database()
 
 export default class NavBar extends Component {
+  constructor() {
+    super()
+    this.state = {
+      language: '',
+    }
+  }
+  
+  changeLanguage = (event, data) => {
+    this.setState({
+      language: data.value,
+    })
+    // using this.state.language below caused race condition?
+    db.ref(`/room`).update({
+      language: data.value
+    })
+  }
 
   render() {
-
+    const languageOptions = [
+      { key: 'Javascript', text: 'Javascript', value: 'javascript' },
+      { key: 'Python', text: 'Python', value: 'python' },
+      { key: 'Ruby', text: 'Ruby', value: 'ruby' },
+      { key: 'Java', text: 'Java', value: 'text/x-java' },
+      { key: 'Go', text: 'Go', value: 'text/x-go' },
+    ]
     return (
       <Menu inverted>
         <Button color='green'>Run</Button>
+        <Dropdown placeholder='Javascript' search selection options={languageOptions} onChange={this.changeLanguage}/>
       </Menu>
     )
   }
